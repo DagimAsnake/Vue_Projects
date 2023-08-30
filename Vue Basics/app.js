4a48cb3e92c27270d23971ff4558d1145d253fbe
Dagim Asnake <dagimasnake1@gmail.com>
2023-08-30 00:40:25 -1200

    solved toggle fav challenge

--- Vue Basics/app.js
+++ Vue Basics/app.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
             x: 0,
             y: 0,
             books: [
-                {title: "name of the wind", author: "patrick rothfuss", img: "assets/1.png", isFav: true},
+                {title: "name of the wind", author: "patrick rothfuss", img: "assets/1.png", isFav: true },
                 {title: "the way of kings", author: "brandon sanderson", img: "assets/2.png", isFav: false},
                 {title: "the final empire", author: "brandon sanderson", img: "assets/3.png", isFav: true}
             ]
@@ -32,6 +32,9 @@ const app = Vue.createApp({
         postionmouse(e) {
             this.x = e.offsetX;
             this.y = e.offsetY;
+        },
+        toggleFav(index) {
+            this.books[index].isFav = !this.books[index].isFav;
         }
     }
   });
