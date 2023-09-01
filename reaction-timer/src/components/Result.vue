da4cf7493288057634adf0881ce1d3d0998c5df5
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-01 03:51:08 -1200

    finished reaction timer game

---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>Reaction time {{score}} ms</p>
+        <p class='rank'>{{rank}}</p>
+    </div>
+</template>
+
+<script>
+export default({
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 200) {
+            this.rank = "Excellent"
+        } else if(this.score < 400) {
+            this.rank = "Very Good"
+        } else {
+            this.rank = "Good"
+        }
+    }
+})
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
